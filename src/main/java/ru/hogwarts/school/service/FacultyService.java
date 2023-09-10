cc4a0f2005ebe1efe37c802114a8295ef86c2043
yarek <yarek2003@mail.ru>
2023-09-10 16:13:27 +0300

    hogwarts.logs

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,32 +17,39 @@ import java.util.List;
 public class FacultyService {
 
     private final FacultyRepository facultyRepository;
-
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
     public Faculty add (Faculty faculty){
+        logger.info("Faculty was created");
         return  facultyRepository.save(faculty);
     }
     public Faculty find (Long idCount) {
+        logger.info("Faculty was found");
         return facultyRepository.findById(idCount).get();
     }
     public Faculty update (Faculty faculty) {
+        logger.info("Faculty was changed");
         return  facultyRepository.save(faculty);
     }
     public void delete(Long idCount) {
+        logger.info("Faculty was removed");
         facultyRepository.deleteById(idCount);
     }
 
     public Collection<Faculty> findByColor(String color) {
+        logger.info("Faculties were found by color");
         return facultyRepository.findByColor(color);
     }
     public Collection<Faculty> findByColorIgnoreCaseOrNameIgnoreCase(String color, String name) {
+        logger.info("Faculties were found by color or name ignore case");
         return facultyRepository.findByColorIgnoreCaseOrNameIgnoreCase(color,name);
     }
 
     public Faculty getFacultyOfStudent(long studentId) {
+        logger.info("Faculty was found by student");
         return facultyRepository.findByStudents_id(studentId);
 
     }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.hogwarts.school.model.Faculty;
@@ -16,43 +18,53 @@ import java.util.List;
 public class StudentService {
 
     private final StudentRepository studentRepository;
-
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public Student add (Student student){
+        logger.info("Student was created");
         return  studentRepository.save(student);
     }
     public Student find (Long idCount) {
+      logger.info("Student was found");
       return studentRepository.findById(idCount).get();
     }
   public Student update (Student student) {
+      logger.info("Student was changed");
       return  studentRepository.save(student);
   }
   public void delete(Long idCount) {
-    studentRepository.deleteById(idCount);
+     logger.info("Student was removed");
+     studentRepository.deleteById(idCount);
   }
 
     public Collection<Student> findByAge(int age) {
+      logger.info("Students were found by age");
       return studentRepository.findByAge(age);
     }
     public Collection<Student> findByAgeBetween(int minAge, int maxAge) {
+        logger.info("Students were found by age between");
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
     public Collection<Student> findStudentsByFaculty_Id(Long id){
+        logger.info("Students were found by faculty");
         return studentRepository.findStudentsByFaculty_Id(id);
     }
 
     public int countStudents() {
+        logger.info("Students were counted");
         return studentRepository.countStudents();
     }
 
     public int averageAgeOfStudents() {
+        logger.info("Average age was calculated");
         return studentRepository.averageAgeOfStudents();
     }
 
     public List<Student> last5Students() {
+        logger.info("Last 5 students were found");
         return studentRepository.last5Students();
     }
 }
